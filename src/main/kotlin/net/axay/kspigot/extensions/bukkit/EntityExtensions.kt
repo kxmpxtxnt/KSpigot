610dbade92b2c1e8f272dae86fa3b26a1e5b1bc2
Paul K <pa179kin@gmail.com>
2025-12-23 12:39:36 +0100

    resolve deprecations

--- src/main/kotlin/net/axay/kspigot/extensions/bukkit/EntityExtensions.kt
+++ src/main/kotlin/net/axay/kspigot/extensions/bukkit/EntityExtensions.kt
@@ -198,7 +198,7 @@ fun Player.give(vararg itemStacks: ItemStack) = inventory.addItem(*itemStacks)
  * Adds all equipment locks to every equipment slot
  */
 fun ArmorStand.fullLock() {
-    for (slot in EquipmentSlot.values()) {
+    for (slot in EquipmentSlot.entries) {
         lock(slot)
     }
 }
@@ -208,7 +208,7 @@ fun ArmorStand.fullLock() {
  * @param slot the slot which gets locked
  */
 fun ArmorStand.lock(slot: EquipmentSlot) {
-    for (lock in ArmorStand.LockType.values()) {
+    for (lock in ArmorStand.LockType.entries) {
         addEquipmentLock(slot, lock)
     }
 }
